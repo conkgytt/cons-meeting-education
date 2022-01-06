39d1802053312e0aea48bc08a4470d442789c4ba
thptnhs.12a12 <thptnhs.12a12@gmail.com>
2022-01-07 00:22:36 +0700

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // Express
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
