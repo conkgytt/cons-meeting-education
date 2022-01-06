f3e7acdd207796ed9718f4534a4cab3f7d337b6f
thptnhs.12a12 <thptnhs.12a12@gmail.com>
2022-01-07 00:39:33 +0700

    first commit

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "POST,GET,LINK");
+});
 
 const routeIndex = require("./routes/routeIndex");
 routeIndex(app);
