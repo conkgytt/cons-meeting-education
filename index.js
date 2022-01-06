d896afc8a76a86659975d2fa335e8510ad103ece
thptnhs.12a12 <thptnhs.12a12@gmail.com>
2022-01-07 00:57:55 +0700

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
 app.use(cors());
 
 require("dotenv").config();
-const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
