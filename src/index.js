516ad46049986ede3fe4dec6d695158546030d69
thptnhs.12a12 <thptnhs.12a12@gmail.com>
2022-01-07 22:57:31 +0700

    new upload

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("static", path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
