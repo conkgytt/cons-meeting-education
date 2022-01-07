b0c5eaea2a4ee11d3c4564c0b82d5b631a33c8ea
thptnhs.12a12 <thptnhs.12a12@gmail.com>
2022-01-07 23:23:46 +0700

    new upload

--- src/routes/routeCreateEvent.js
+++ src/routes/routeCreateEvent.js
@@ -20,9 +20,11 @@ router.post("/", (req, res, next) => {
 		attendees,
 		start: {
 			dateTime: startAt,
+			timeZone: "+7",
 		},
 		end: {
 			dateTime: endAt,
+			timeZone: "+7",
 		},
 		conferenceData: {
 			createRequest: {
